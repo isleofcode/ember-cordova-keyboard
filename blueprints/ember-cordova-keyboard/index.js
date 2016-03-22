7281aef8454369ebf74fafe797b9d42ff478a501
Chris Thoburn <runspired@gmail.com>
2016-03-22 02:58:05 -0500

    feat(plugin): auto installs plugin

---
+++ blueprints/ember-cordova-keyboard/index.js
@@ -0,0 +1,25 @@
+/*jshint node:true*/
+var awaitCommand = require('../../lib/await-command');
+var VersionChecker = require('ember-cli-version-checker');
+
+module.exports = {
+  description: 'Installs the cordova ionic keyboard plugin',
+
+  normalizeEntityName: function() {},
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  afterInstall: function() {
+    var checker = new VersionChecker(this);
+    var dep = checker.for('ember-platform-cordova', 'npm');
+    if (!dep.version) {
+      throw new Error("ember-cordova-keyboard requires ember-platform-cordova.");
+    }
+    return awaitCommand('ember cordova plugin add ionic-plugin-keyboard --save');
+  }
+};
