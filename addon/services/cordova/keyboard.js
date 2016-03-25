f270141935c9b1e3c23d99121b3c80b3d45c3b6e
@anulman <anulman@gmail.com>
2016-03-24 23:25:09 -0400

    refactor(keyboard-service): rm unnecessary comments and tidy service

--- addon/services/cordova/keyboard.js
+++ addon/services/cordova/keyboard.js
@@ -2,37 +2,43 @@
 import Ember from 'ember';
 
 const {
+  A,
+  Evented,
+  RSVP,
   Service,
   inject,
-  Evented,
-  RSVP
+  run
 } = Ember;
 
-const {
-  Promise
-} = RSVP;
-
-const KEYBOARD_ANIMATION_TIME = 300; //ms, guestimated based on various SO issues
-
-
+const { Promise } = RSVP;
+const KEYBOARD_ANIMATION_TIME = 300; //ms, guestimated from SO recommendations
 
 export default Service.extend(Evented, {
+  cordova: inject.service('cordova'),
 
   adjustBodyHeight: true,
   shouldDisableScroll: true,
   keyboardHeight: 0,
 
-  cordova: inject.service('cordova'),
+  _listeners: [],
+  _height: null,
 
-  /*
-    We always access the keyboard via promise to ensure
-    our interaction with it possible.
-   */
+  init() {
+    this._super();
+
+    this._listeners = new A();
+    this.keyboard().then(kb => { this.setup(kb); });
+  },
+
+  willDestroy() {
+    this._super();
+    this.teardownListeners();
+  },
+
   _keyboard: null,
   keyboard() {
-    if (this._keyboard) {
-      return Promise.resolve(this._keyboard);
-    }
+    if (this._keyboard) { return Promise.resolve(this._keyboard); }
+
     return this.get('cordova').ready()
       .then(() => {
         this._keyboard = cordova.plugins.Keyboard;
@@ -40,140 +46,84 @@ export default Service.extend(Evented, {
       });
   },
 
-
-  /*
-    Opens the keyboard, unlike cordova.keyboard.open
-    this method returns a promise that guestimates
-    when the keyboard is done opening.
-
-    Also, unlike cordova.keyboard, this method can
-    accepts an element and will call focus on it.
-   */
   open(element) {
-    return this.keyboard()
-      .then((kb) => {
-        if (kb.isVisible) {
-          if ("activeElement" in document && document.activeElement !== element) {
-            element.focus();
-          }
+    return this.keyboard().then(kb => {
+      const elementShouldFocus = "activeElement" in document &&
+        document.activeElement !== element;
 
-          return true;
-        }
+      if (kb.isVisible) {
+        if (elementShouldFocus) { element.focus(); }
+        return true;
+      }
 
-        return new Promise((resolve) => {
-          if ("activeElement" in document && document.activeElement !== element) {
-            element.focus();
-          }
-          run.later(() => { resolve(); }, KEYBOARD_ANIMATION_TIME);
-        });
+      return new Promise(resolve => {
+        if (elementShouldFocus) { element.focus(); }
+        run.later(() => { resolve(); }, KEYBOARD_ANIMATION_TIME);
       });
+    });
   },
 
-
-  /*
-   Closes the keyboard, unlike cordova.keyboard.open
-   this method returns a promise that guestimates
-   when the keyboard is done opening.
-   */
   close() {
-    return this.keyboard()
-      .then((kb) => {
-        if (!kb.isVisible) {
-          return true;
-        }
+    return this.keyboard().then(kb => {
+      if (!kb.isVisible) { return true; }
 
-        return new Promise((resolve) => {
-          kb.close();
-          run.later(() => { resolve(); }, KEYBOARD_ANIMATION_TIME);
-        });
+      return new Promise(resolve => {
+        kb.close();
+        run.later(() => { resolve(); }, KEYBOARD_ANIMATION_TIME);
       });
+    });
   },
 
-
-  disableScroll(v) {
-    this.keyboard()
-      .then((kb) => {
-        this.set('shouldDisableScroll', v);
-        kb.disableScroll(v);
-      });
+  disableScroll(bool) {
+    this.keyboard().then((kb) => {
+      this.set('shouldDisableScroll', bool);
+      kb.disableScroll(bool);
+    });
   },
 
-
-  /*
-   * Create a listener for each default Cordova event
-   * and have it trigger the event on the service
-   */
-  _listeners: null,
-  _height: null,
   setup(kb) {
-    if (this._listeners) {
-      return;
-    }
-    this._listeners = [];
+    const onKeyboardShow = this.onKeyboardShow.bind(this),
+          onKeyboardHide = this.onKeyboardHide.bind(this),
+          listeners = [
+            { name: 'native.keyboardshow', fn: onKeyboardShow },
+            { name: 'native.keyboardhide', fn: onKeyboardHide }
+          ];
 
     kb.disableScroll(this.get('shouldDisableScroll'));
 
-    // add an additional handler for native.keyboardshow
-    // ensure it fires first
-    let listener = {
-      name: 'native.keyboardshow',
-      method: (e) => {
-        this.set('keyboardHeight', e.keyboardHeight);
-
-        if (this.get('adjustBodyHeight')) {
-          this._height = document.body.style.height || '';
-          document.body.style.height = "calc(100% - " + e.keyboardHeight + "px)";
-        }
-
-        this.trigger(name, e);
-      }
-    };
-    this._listeners.push(listener.name, listener.method, true);
-
-    // add an additional handler for native.keyboardhide
-    // ensure it fires first
-    listener = {
-      name: 'native.keyboardhide',
-      method: (e) => {
-        this.set('keyboardHeight', 0);
-
-        if (this.get('adjustBodyHeight')) {
-          document.body.style.height = this._height;
-        }
-
-        this.trigger(name, e);
-      }
-    };
-    this._listeners.push(listener.name, listener.method, true);
-
-    this._listeners.forEach((l) => {
-      window.addEventListener(l.name, l.method, true);
+    listeners.forEach(listener => {
+      this._listeners.pushObject(listener);
+      window.addEventListener(listener.name, listener.fn, true);
     });
   },
 
+  onKeyboardShow(e) {
+    this.set('keyboardHeight', e.keyboardHeight);
+
+    if (this.get('adjustBodyHeight')) {
+      this._height = document.body.style.height || '';
+      document.body.style.height = "calc(100% - " + e.keyboardHeight + "px)";
+    }
+
+    this.trigger('keyboardDidShow', e);
+  },
+
+  onKeyboardHide(e) {
+    this.set('keyboardHeight', 0);
+
+    if (this.get('adjustBodyHeight')) {
+      document.body.style.height = this._height;
+    }
+
+    this.trigger('keyboardDidHide', e);
+  },
 
   teardownListeners() {
-    if (!this._listeners) {
-      return;
-    }
-    this._listeners.forEach((listener) => {
-      window.removeEventListener(listener.name, listener.method, true);
+    const listeners = this._listeners;
+
+    listeners.forEach(listener => {
+      window.removeEventListener(listener.name, listener.fn, true);
+      this._listeners.removeObject(listener);
     });
   },
-
-
-  willDestroy() {
-    this._super();
-    this.teardownListeners();
-  },
-
-
-  init() {
-    this._super();
-    this.keyboard()
-      .then((kb) => {
-        this.setup(kb);
-      });
-  }
-
 });
